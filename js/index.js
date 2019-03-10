ad783d9506774da3ad7fefc61426ed1f5e4a18d4
Nitesh Thakur <niteshthakur498@gmail.com>
2019-03-11 02:59:47 +0530

    Initialised

---
+++ js/index.js
@@ -0,0 +1,56 @@
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var clubs = document.querySelectorAll('.clubs-con-img');
+    
+    clubs.forEach(club=>{
+        club.addEventListener('click', function(){
+            popupBack.style.opacity = 1;
+            popupBack.style.visibility = 'visible';
+        })
+    })
+
+    var close = document.querySelector('.popup__close');
+    var popupBack = document.querySelector('.popup');
+    console.log(close);
+    close.addEventListener('click', function(){
+        popupBack.style.opacity = 0;
+        popupBack.style.visibility = 'hidden';
+    });
+    $('.clubs-con-img').each(function(i){
+      var row = $(this);
+      setTimeout(function() {
+        row.css('transform',"translate(0px)");
+        row.css('opacity','1');
+      }, 500*i)
+    });
+});
+
+
+// $('[has-ripple="true"]').click(function () {
+// $(this).toggleClass('clicked');
+// $('.menu').toggleClass('open');
+// });
+
+// $(document).ready()
+
+// $('.menu a').each(function (index) {
+// var thismenuItem= $(this);
+
+// thismenuItem.click(function (event) {
+//     event.preventDefault();
+//     console.log('hey');
+    // $('.menuitem-wrapper').eq(index).addClass('spin');
+    
+    // var timer = setTimeout(function () {
+    // $('body').removeAttr('class').addClass('bg-'+index);
+    // $('.menuitem-wrapper').eq(index).removeClass('spin');
+    // $('.menu').removeClass('open');
+    // $('.menu-btn').removeClass('clicked');
+    // }, 800);
+// });
+// });
+// $('navigation_icon').click(function(){
+//     console.log('hey');
+// })
---
+++ server.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var User = require('./userSchema.js');
+//Creating Server using express()
+var app = express();
+
+//use method for static files
+app.use(express.static(__dirname));
+
+//Connecting to mongodb loaclhost database
+mongoose.connect(process.env.MONGODB_URI||"mongodb://localhost:27017/nimbus" ,{ useNewUrlParser: true });
+
+
+//Using Body parser for middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({strict:false}));
+
+
+app.get('/register',function (req,res){
+  console.log("okay");
+  res.sendFile(__dirname+"/register.html");
+});
+
+app.get('/events',function (req,res){
+  console.log("okay");
+  res.sendFile(__dirname+"/events.html");
+});
+app.get('/',function (req,res){
+  console.log("okay");
+  res.sendFile(__dirname+"/main.html");
+});
+app.get('/team',function (req,res){
+  console.log("okay");
+  res.sendFile(__dirname+"/team.html");
+});
+//Post Method
+app.post('/adduser',function(req, res){
+    var data = req.body;
+    console.log(req.body.team);
+      var user=new User(data);
+      console.log(user);
+      user.save(function(err,user,numAffected){
+      if(err){
+            console.log(err);
+            res.send('500');
+          }
+       else{
+        console.log("added",user);
+        res.send(user);
+       }
+});
+});
+
+// Listening to port-number
+app.listen(3000,function(){
+    console.log("localhost at 3000");
+});
